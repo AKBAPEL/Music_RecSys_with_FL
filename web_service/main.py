0633695cc050e7691dbd2ab781aceec92a859fd8
Хамаганов Ильдар Александрович <iakhamaganov@edu.hse.ru>
2025-05-10 17:25:33 +0300

    added fit ruchka

---
+++ web_service/main.py
@@ -0,0 +1,156 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from sklearn.preprocessing import LabelEncoder
+from scipy.sparse import csr_matrix
+import numpy as np
+import implicit
+from pydantic import BaseModel
+import multiprocessing
+from datetime import datetime
+import time
+from pathlib import Path
+import uuid
+
+TRAIN = f"../train.csv"
+train_df = pd.read_csv(TRAIN)
+
+class FitRequest(BaseModel):
+    model_name: str
+    model_type: str
+    factors: int = 100
+    iterations: int = 20
+    regularization: float = 0.01
+    alpha: float = 1.0
+
+ml_models = {}
+
+def get_als_data(train_df):
+    le_msno = LabelEncoder()
+    le_song_id = LabelEncoder()
+
+    le_msno.fit(train_df["msno"])
+    le_song_id.fit(train_df["song_id"])
+
+    train = pd.DataFrame(
+        {"msno": le_msno.transform(train_df["msno"].values), "song_id": le_song_id.transform(train_df["song_id"].values),
+        "target": train_df["target"]})
+
+    user_ids = train["msno"].values
+    song_ids = train["song_id"].values
+    target = train["target"].values
+
+    matrix = csr_matrix((target, (user_ids, song_ids)), shape=(len(train['msno'].unique()), len(train['song_id'].unique())))
+
+    ml_models['le_song_id'] = le_song_id
+    return matrix, le_msno, le_song_id, train
+
+matrix, le_msno, le_song_id, train = get_als_data(train_df=train_df)
+
+
+def music_recommendation(user_id, n=10):
+    with open("prepared_models/model_als.pkl", 'rb') as model_file:  # experiment-als модель
+        loaded_model = pickle.load(model_file)
+
+    recommendations = loaded_model.recommend(user_id, matrix[user_id], N=n)
+    res = le_song_id.inverse_transform(recommendations[0].tolist())
+    return list(res)
+    
+@asynccontextmanager
+async def ml_lifespan_manager(app: FastAPI):
+    ml_models["music_recommendation"] = music_recommendation
+    yield
+    ml_models.clear()
+
+app = FastAPI(lifespan=ml_lifespan_manager)
+
+@app.get("/")
+async def root():
+    return {"message" : "Hi, User!"}
+
+
+@app.post("/predict")
+async def predict(user_id: int, n: int = 10):
+    global matrix
+
+    if user_id >= matrix.shape[0]:  # новый пользователь
+        user_id = matrix.shape[0] - 1  # временное решение по определению нового пользователя
+
+        top_10_songs = train["song_id"].value_counts().head(10).index
+        new_user_data = np.array(np.ones(len(top_10_songs)))
+        new_user_indices = np.array(top_10_songs)
+        new_data = np.concatenate([matrix.data, new_user_data])
+        new_indices = np.concatenate([matrix.indices, new_user_indices])
+        new_indptr = np.concatenate([matrix.indptr, [matrix.indptr[-1] + len(new_user_data)]])
+
+        matrix = csr_matrix((new_data, new_indices, new_indptr), shape=(matrix.shape[0] + 1, matrix.shape[1]))
+
+    return ml_models["music_recommendation"](user_id, n)
+
+
+def _train_model(params: FitRequest, model_id: str):
+    try:
+        # start_time = time.time()
+
+        model = implicit.als.AlternatingLeastSquares(
+            factors=params.factors,
+            iterations=params.iterations,
+            regularization=params.regularization,
+            alpha=params.alpha,
+            random_state=42
+        )
+        model.fit(matrix)
+        with open(f"users_models/{params.model_name}_{params.model_type}_{model_id}.pkl", 'wb') as f:
+            pickle.dump(model, f)
+
+        # logger.info(f"Model {model_id} trained successfully")
+        return True
+    except Exception as e:
+        # logger.error(f"Training failed: {e}")
+        return False
+
+@app.post("/fit", response_model=dict)
+async def fit_model(request: FitRequest):
+    # model_id = f"model_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    model_id = str(uuid.uuid4())
+
+    p = multiprocessing.Process(
+        target=_train_model,
+        args=(request, model_id)
+    )
+    p.start()
+
+    start_time = time.time()
+    timeout = 30
+    while time.time() - start_time < timeout:
+        if not p.is_alive():
+            break
+        time.sleep(0.1)
+ 
+    if p.is_alive():
+        p.terminate()
+        # logger.warning(f"Training timeout for model {model_id}")
+        return {
+            "status": "error",
+            "message": "Training timeout"
+        }
+
+
+    if not Path(f"users_models/{request.model_name}_{request.params.model_type}_{model_id}.pkl").exists():
+        return {
+            "status": "error",
+            "message": "Training failed"
+        }
+
+    return {
+        "status": "success",
+        "model_id": model_id,
+        "details": {
+            "factors": request.factors,
+            "iterations": request.iterations,
+            "regularization": request.regularization
+        },
+        "message": "Model trained and saved"
+    }

68a920e67e81f96d03b6a0860a496352c36aee55
Хамаганов Ильдар Александрович <iakhamaganov@edu.hse.ru>
2025-05-10 21:04:12 +0300

    pydantic models + minor fixes

--- web_service/patterns.py
+++ web_service/patterns.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-
+from typing import List, Optional, Dict
 class FitRequest(BaseModel):
     model_name: str
     model_type: str
     factors: int = 100
-    iterations: int = 20
+    iterations: int = 1 # 20
     regularization: float = 0.01
     alpha: float = 1.0
 
@@ -14,3 +14,19 @@ class ModelInfo(BaseModel):
     model_path: str
     # is_active: bool = False
 
+class FitResponse(BaseModel):
+    status: str
+    model_id: str
+    message: str
+    details: dict
+
+class SetResponse(BaseModel):
+    status: str
+    active_model: str
+
+class ErrorResponse(BaseModel):
+    detail: str
+
+class PredictResponse(BaseModel):
+    recommendations: List[str]
+    user_type: str
